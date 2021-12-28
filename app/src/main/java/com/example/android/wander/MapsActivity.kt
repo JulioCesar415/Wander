2b64bc174f676e4f5f0a9b617ecd0112b00a6a66
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-12-27 19:01:24 -0800

    program created

--- app/src/main/java/com/example/android/wander/MapsActivity.kt
+++ app/src/main/java/com/example/android/wander/MapsActivity.kt
@@ -24,7 +24,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
+//                SupportMapFragment is a way to get google map in application
             .findFragmentById(R.id.map) as SupportMapFragment
+//        .getMapAsync method helps prepare google map
         mapFragment.getMapAsync(this)
     }
 
@@ -37,12 +39,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+//    onMapReady method is called when map is loaded
     override fun onMapReady(googleMap: GoogleMap) {
+//        set map to google map that is passed in
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
+//        create latitude and longitude object
         val sydney = LatLng(-34.0, 151.0)
+//        add marker with position at sydney with title Marker in Sydney
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        move camera to sydney
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
